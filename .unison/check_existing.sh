d3dc3a0fdf1de2f9acd6a43b98bb322170d72269
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-02-08 21:08:31 +0200

    Commit 2024-02-08 21:08

--- .unison/check_existing.sh
+++ .unison/check_existing.sh
@@ -6,14 +6,15 @@
 file="$1"
 root="$HOME"
 
-notexist="Not_existing_paths_$file"
-echo "" > "$notexist"
+notexist="Not_existing_paths_$file.temp"
 
-grep "^[ ]*path[ ]*=.*" "$file" |\
-    cut -d'=' -f2-  |\
-    sed 's/^[ ]*//' |\
+## get include paths
+echo "##  INCLUDED PATHS  ##" > "$notexist"
+echo "                      " >> "$notexist"
+grep "^[# ]*path[ ]*=.*" "$file" |\
+    cut -d'=' -f2-   |\
+    sed 's/^[# ]*//' |\
     while read line; do
-
         if [ -e "$root/$line" ]; then
            echo "EXIST: $line"
         else
@@ -21,6 +22,23 @@ grep "^[ ]*path[ ]*=.*" "$file" |\
            echo "$line" >> "$notexist"
         fi
     done
+
+## get ignored paths
+(
+    echo "                      "
+    echo "##  EXCLUDED PATHS  ##"
+    echo "                      " ) >> "$notexist"
+grep "^[# ]*ignore[ ]*=[ ]*[Pp]ath[ ]*" "$file" |\
+    sed 's/^[^{]*{\([^{}]*\)}.*/\1/' |\
+    while read line; do
+        if [ -e "$root/$line" ]; then
+           echo "EXIST: $line"
+        else
+           echo "NOT EXISTING: $line"
+           echo "$line" >> "$notexist"
+        fi
+    done
+
 
 echo ""
 echo "NOT EXISTING PATHS"
