c6390941093150eb22c41440f2bedf5267ad2325
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-12-06 10:13:12 +0200

    Commit 2025-12-06 10:13

--- .config/sxhkd/keybinds.sh
+++ .config/sxhkd/keybinds.sh
@@ -2,21 +2,49 @@
 
 sxhkdfile="/home/athan/.config/sxhkd/sxhkdrc"
 
-awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$sxhkdfile"  | column -t -s $'\t' 
+(
+awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$sxhkdfile" |
+  tr "#" " " |
+  tr -s ' '  |
+  sort -V
 
-grep "^bindsym" ~/.config/i3/config | sed 's/-\(-\w\+\)\+//g;s/$mod/Win/g;s/Mod1/Alt/g;s/exec //; s/bindsym //; s/^\s\+//;s/^\([^ ]\+\) \(.\+\)$/<small> \1: \2/<\/small>; s/^\s\+//'  | tr ":" "\t"
+grep "^bindsym" ~/.config/i3/config |
+  sed 's/-\(-\w\+\)\+//g;s/$mod/mod4/g; s/Mod1/Alt/g; s/exec //; s/bindsym //; s/^\s\+//; s/^\([^ ]\+\) \(.\+\)$/<small> \1: \2<\/small>/; s/^\s\+//' |
+  tr -s ' '  |
+  tr ":" "\t"|
+  sort -V
+) | column -t -s $'\t'
 
-exit
-if [[ $HOST == "tyler" ]]; then
-    awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$configfile" |
-        column -t -s $'\t' |
-        sort -V            |
-        rofi -config "/home/athan/.config/rofi/config"  -dmenu -i -markup-rows -no-show-icons -lines 20  -font "monospace 11"
-else
-    awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$configfile" |
-        column -t -s $'\t' |
-        sort -V            |
+
+if [[ -f "/home/athan/.config/rofi/config" ]]; then
+
+  (
+    awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$sxhkdfile" |
+      tr "#" " " |
+      tr -s ' '  |
+      sort -V
+
+    grep "^bindsym" ~/.config/i3/config |
+      sed 's/-\(-\w\+\)\+//g;s/$mod/mod4/g; s/Mod1/Alt/g; s/exec //; s/bindsym //; s/^\s\+//; s/^\([^ ]\+\) \(.\+\)$/<small> \1: \2<\/small>/; s/^\s\+//' |
+      tr -s ' '  |
+      tr ":" "\t"|
+      sort -V
+    ) | column -t -s $'\t' |
+      rofi -config "/home/athan/.config/rofi/config"  -dmenu -i -markup-rows -no-show-icons -lines 20  -font "monospace 11"
+  else
+
+    (
+      awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' "$sxhkdfile" |
+        tr "#" " " |
+        tr -s ' '  |
+        sort -V
+
+      grep "^bindsym" ~/.config/i3/config |
+        sed 's/-\(-\w\+\)\+//g;s/$mod/mod4/g; s/Mod1/Alt/g; s/exec //; s/bindsym //; s/^\s\+//; s/^\([^ ]\+\) \(.\+\)$/<small> \1: \2<\/small>/; s/^\s\+//' |
+        tr -s ' '  |
+        tr ":" "\t"|
+        sort -V
+      ) | column -t -s $'\t' |
         rofi -dmenu -i -markup-rows -no-show-icons -lines 20  -font "monospace 11"
 fi
-
 
